76ea166ecc16cca7d26a52025a8f7a56e56a484f
unknown <ms5978307@gmail.com>
2020-05-19 11:50:47 -0700

    Updates

---
+++ precache-manifest.62210c36ff5e3f0773d9be5d5de7b519.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfff1f7a114f2ab55ac0160c0bbaa164",
+    "url": "/animated-menu-from-tutorial/index.html"
+  },
+  {
+    "revision": "ea2aea6b34a1a1863680",
+    "url": "/animated-menu-from-tutorial/static/css/main.263c26da.chunk.css"
+  },
+  {
+    "revision": "c36aa0fadef873488271",
+    "url": "/animated-menu-from-tutorial/static/js/2.8fb1944b.chunk.js"
+  },
+  {
+    "revision": "723894b771c1fe011b0df55ec646ea2d",
+    "url": "/animated-menu-from-tutorial/static/js/2.8fb1944b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea2aea6b34a1a1863680",
+    "url": "/animated-menu-from-tutorial/static/js/main.786946a1.chunk.js"
+  },
+  {
+    "revision": "d2738ffdee698b393181",
+    "url": "/animated-menu-from-tutorial/static/js/runtime-main.50872ba7.js"
+  },
+  {
+    "revision": "a7aadf3337b13c1d1811e42a8f2b6c66",
+    "url": "/animated-menu-from-tutorial/static/media/austin.a7aadf33.webp"
+  },
+  {
+    "revision": "209dcd10075f7628d6cb393e72e4939c",
+    "url": "/animated-menu-from-tutorial/static/media/beijing.209dcd10.webp"
+  },
+  {
+    "revision": "2bc381878816686be22cef7037544540",
+    "url": "/animated-menu-from-tutorial/static/media/dallas.2bc38187.webp"
+  },
+  {
+    "revision": "ebb8196532e1924ad86530b2cde4c220",
+    "url": "/animated-menu-from-tutorial/static/media/newyork.ebb81965.webp"
+  },
+  {
+    "revision": "b58ad59e1fd1f34d49ef692f3ff3ff8d",
+    "url": "/animated-menu-from-tutorial/static/media/sanfrancisco.b58ad59e.webp"
+  }
+]);
